7a28f9cf8eb6b2bc854d6c5f6057302f6ee76191
Ali Developer <aliameer1977@gmail.com>
2018-08-05 08:44:23 -0700

    First commit

---
+++ register.js
@@ -0,0 +1,12 @@
+
+// resources:https://developers.google.com/web/fundamentals/primers/service-workers/
+
+// Regiter serviceWorker
+if ('serviceWorker' in navigator) {
+       navigator.serviceWorker
+       .register('sw.js/index.js')
+       .then(()=> {
+       console.log("Service Worker Registered");
+
+ });
+}
