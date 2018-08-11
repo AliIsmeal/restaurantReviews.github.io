2706d0b8b0e126bfbf7d78b6474ab59885868d56
Ali Developer <aliameer1977@gmail.com>
2018-08-10 17:47:49 -0700

    style:formatting, no code change

--- js/main.js
+++ js/main.js
@@ -4,8 +4,7 @@ let restaurants,
 var newMap
 var markers = []
 
-/**
- * Fetch neighborhoods and cuisines as soon as the page is loaded.
+/*** Fetch neighborhoods and cuisines as soon as the page is loaded.
  */
 document.addEventListener('DOMContentLoaded', (event) => {
   initMap(); // added
@@ -31,7 +30,7 @@ fetchNeighborhoods = () => {
  * Set neighborhoods HTML.
  */
 fillNeighborhoodsHTML = () => {
-    let neighborhoods = self.neighborhoods;
+  let neighborhoods = self.neighborhoods;
   const select = document.getElementById('neighborhoods-select');
   neighborhoods.forEach(neighborhood => {
     const option = document.createElement('option');
@@ -74,12 +73,12 @@ fillCuisinesHTML = (cuisines = self.cuisines) => {
  */
 initMap = () => {
   self.newMap = L.map('map', {
-      center: [40.722216, -73.987501],
-        zoom: 12,
-        scrollWheelZoom: false
-      });
+    center: [40.722216, -73.987501],
+    zoom: 12,
+    scrollWheelZoom: false
+  });
   L.tileLayer('https://api.tiles.mapbox.com/v4/{id}/{z}/{x}/{y}.jpg70?access_token={mapboxToken}', {
-      mapboxToken:'pk.eyJ1IjoiYWxpZGV2ZWxvcGVyIiwiYSI6ImNqanp5ZzUzdjAxNzkzcHBxdmJpdHFjeWsifQ.SQSBQu2uw9SE0pG61K4ZmQ',
+    mapboxToken: 'pk.eyJ1IjoiYWxpZGV2ZWxvcGVyIiwiYSI6ImNqanp5ZzUzdjAxNzkzcHBxdmJpdHFjeWsifQ.SQSBQu2uw9SE0pG61K4ZmQ',
     maxZoom: 18,
     attribution: 'Map data &copy; <a href="https://www.openstreetmap.org/">OpenStreetMap</a> contributors, ' +
       '<a href="https://creativecommons.org/licenses/by-sa/2.0/">CC-BY-SA</a>, ' +
@@ -192,20 +191,21 @@ addMarkersToMap = (restaurants = self.restaurants) => {
     // Add marker to the map
     const marker = DBHelper.mapMarkerForRestaurant(restaurant, self.newMap);
     marker.on("click", onClick);
+
     function onClick() {
       window.location.href = marker.options.url;
     }
     self.markers.push(marker);
   });
 
-//}
-//addMarkersToMap = (restaurants = self.restaurants) => {
-//  restaurants.forEach(restaurant => {
-//    // Add marker to the map
-//    const marker = DBHelper.mapMarkerForRestaurant(restaurant, self.map);
-//    google.maps.event.addListener(marker, 'click', () => {
-//      window.location.href = marker.url
-//    });
-//    self.markers.push(marker);
-//  });
+  //}
+  //addMarkersToMap = (restaurants = self.restaurants) => {
+  //  restaurants.forEach(restaurant => {
+  //    // Add marker to the map
+  //    const marker = DBHelper.mapMarkerForRestaurant(restaurant, self.map);
+  //    google.maps.event.addListener(marker, 'click', () => {
+  //      window.location.href = marker.url
+  //    });
+  //    self.markers.push(marker);
+  //  });
 }
